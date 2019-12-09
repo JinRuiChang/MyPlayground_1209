3beedfb99d60d698e418ab8447d919fb52a3f47e
CHANG JINRUI <jrc067@gmail.com>
2019-12-09 10:34:26 +0800

    first commit

---
+++ MyPlayground_1209.playground/Contents.swift
@@ -0,0 +1,7 @@
+import UIKit
+var rect = CGRect(x: 0, y: 0, width: 200, height: 134)
+var rect2 = CGRect(x: 0, y: 0, width: 200, height: 134)
+let backgroungView = UIView(frame: rect)
+backgroungView.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
+//
+

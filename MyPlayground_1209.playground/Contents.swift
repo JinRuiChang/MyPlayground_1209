47e80089ff35fe6261a58b53fb6b0438e25c98cd
CHANG JINRUI <jrc067@gmail.com>
2019-12-09 10:34:57 +0800

    test

--- MyPlayground_1209.playground/Contents.swift
+++ MyPlayground_1209.playground/Contents.swift
@@ -4,4 +4,4 @@ var rect2 = CGRect(x: 0, y: 0, width: 200, height: 134)
 let backgroungView = UIView(frame: rect)
 backgroungView.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
 //
-
+let a = true
